1fe0de2656def77cf77191b600d39da795db6c53
132-jwang <55249853+132-jwang@users.noreply.github.com>
2019-09-16 10:02:35 -0700

    Add files via upload

---
+++ mad lips JasonW.py
@@ -0,0 +1,25 @@
+adj1 = input("give me a adjective:")
+pnoun = input("give me a plural noun:")
+noun = input("give me a noun:")
+adv = input("give me a adverb:")
+nub = input("give me a number:")
+pverb = input("give me a past tense verb:")
+estadj = input("give me a -est adjective:")
+pverb2 = input("give me a past tense verb:")
+
+
+print("Today, my fabulous  camp group went to a (an) " + adj1 +
+      " amusement park. It  was a fun park with lots of cool " +pnoun+
+      "and enjoyable play structures. When we got there,"
+      "my kind counselor shouted loudly,"
+     " Everybody off:"  + noun + " the We all pushed out in a terrible hurry."
+     " My counselor handed out yellow tickets, and we scurried in."
+      "I was so excited! I couldn't figure out what exciting thing to do first."
+      "I saw a scary roller coaster I really liked so, I " + adv +
+      " ran over to get in the long line that had about" + nub +
+      " people in it. When I finally got on the roller coaster I was" + pverb +
+      ".In fact I was so nervous my two knees were knocking"
+      "together. This was the " + estadj +  " ride I had ever been on! In"
+      "about two minutes I heard the crank and grinding of the gears."
+     " That’s when the ride began! When I got to the bottom, I was a little"
+       + pverb2 + " but I was proud of myself.")
---
+++ price and tax JasonW.py
@@ -0,0 +1,7 @@
+item1 = input("please enter the price of item1:")
+item2 = input("please enter the price of item2:")
+item3 = input("please enter the price of item3:")
+salestax = (float(item1)+float(item2)+float(item3))*0.12
+total = (float(item1)+float(item2)+float(item3))*1.12
+print("The total sales tax is ${0:.2f}".format(salestax))
+print("The total cost is ${0:.2f}".format(total))
