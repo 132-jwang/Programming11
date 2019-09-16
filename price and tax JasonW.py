225bed0862d1c06b2f8563251ed4a9572db8cb67
132-jwang <55249853+132-jwang@users.noreply.github.com>
2019-09-16 10:17:27 -0700

    Update price and tax JasonW.py

--- price and tax JasonW.py
+++ price and tax JasonW.py
@@ -1,4 +1,4 @@
-item1 = input("please enter the price of item1:")
+item1 = input("please enter the price of item1:") #good
 item2 = input("please enter the price of item2:")
 item3 = input("please enter the price of item3:")
 salestax = (float(item1)+float(item2)+float(item3))*0.12
