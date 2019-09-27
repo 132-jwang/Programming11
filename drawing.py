eff59a9e71224e487bf8bf83f6b4124428d6f00b
132-jwang <55249853+132-jwang@users.noreply.github.com>
2019-09-27 10:11:43 -0700

    Add files via upload

--- drawing.py
+++ drawing.py
@@ -1,6 +1,7 @@
 import turtle
 x=0
 y=0
+turtle.bgcolor("black")
 def star(x,y,color1,color2):
     turtle.speed(10)
     turtle.pu()
@@ -16,23 +17,31 @@ def star(x,y,color1,color2):
 
 a=0
 b=0
-def trig(a,b,co1,co2):
+def trig(a,b):
     turtle.pu()
     turtle.setposition(a,b) #set the position of the pen
     turtle.pd()
-    turtle.color(co1,co2)
+    turtle.color("brown","grey")
     counter=0
+    
     turtle.begin_fill()
     while counter <3: #draw a triangle
         turtle.right(120)
         turtle.forward(100)
         counter=counter+1
     turtle.end_fill()
-    
+
+for trigs in range (4): #duplicate the triangles
+    trig(a,b)
+    a=a-100
+
+turtle.ht()
+
     
 
 star(-200,250,"yellow","red") #define the function
 star(200,200,"blue","green")
 star(-400,250,"purple","pink")
-trig(0,-100,"brown","black")
+trig(0,0)
+
 
