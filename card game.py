9a62bfdafdb50e20ed16685512d2fe544c46e237
132-jwang <55249853+132-jwang@users.noreply.github.com>
2019-11-28 09:48:27 -0800

    Add files via upload

--- card game.py
+++ card game.py
@@ -1,17 +1,19 @@
 import random
 import time
-done= False
-start = False
-end = False
+start = True
 
-balance =100
-cbalance = 100
+counter = 1
+
+balance =10
+cbalance = 10
 suit=('Spades','Hearts','Diamond','Clubs')
-if start == False:
+
+
+
+while counter == 1:
     print("Dealing the cards")
-    start= True
-time.sleep(2)
-if done == False:
+    
+    time.sleep(2)
 
     rad=random.choice(suit)
     radd=random.randint(1,13)
@@ -20,30 +22,29 @@ if done == False:
     rad2=random.choice(suit)
     radd2=random.randint(1,13)
     print('Computer:',rad2,radd2)
+    time.sleep(1)
 
-    done = True
-    end= True
+    if radd> radd2:
+        print('You win')
+        balance +=5
+        cbalance -=5
+        print('your balance is',balance)
 
-if radd> radd2:
-    print('You win')
-    balance +=5
-    cbalance -=5
-    print('your balance is',balance)
+    if radd< radd2:
+        print('You Lose')
+        balance -=5
+        cbalance +=5
+        print('your balance is',balance)
 
-if radd< radd2:
-    print('You Lose')
-    balance -=5
-    cbalance +=5
-    print('your balance is',balance)
 
-while end == True:
-    play=print(input('Do you want to continue playing? (Yes/No)'))
-    again =input('Do you want to play again?')
-    print(again)
-    if again =='yes':
-        done=False
-        start=False
-    if again == 'no':
-        print('Thank you for playing')
-    end=False
+    if balance == 0:
+        print("You lose the game!")
+        counter == 0
+        break
 
+    if balance==20:
+        print('You win the game!')
+        counter == 0
+        break
+    
+    
